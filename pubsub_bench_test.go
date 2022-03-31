55ebe24f59b01572a9a2d52560cbae55538a0f90
Dege <dewadg@dewadg.id>
2022-03-31 14:47:00 +0700

    test: update bench

--- pubsub_bench_test.go
+++ pubsub_bench_test.go
@@ -7,17 +7,33 @@ import (
 
 func BenchmarkPublish_int(b *testing.B) {
 	t := DeclareTopic[int]()
+	outputChan := make(chan int, b.N)
+
+	t.Subscribe(func(ctx context.Context, p int) error {
+		outputChan <- p
+		return nil
+	})
 
 	for i := 0; i < b.N; i++ {
 		_ = t.Publish(context.Background(), 5)
+
+		<-outputChan
 	}
 }
 
 func BenchmarkPublish_string(b *testing.B) {
 	t := DeclareTopic[string]()
+	outputChan := make(chan string, b.N)
+
+	t.Subscribe(func(ctx context.Context, p string) error {
+		outputChan <- p
+		return nil
+	})
 
 	for i := 0; i < b.N; i++ {
 		_ = t.Publish(context.Background(), "test")
+
+		<-outputChan
 	}
 }
 
@@ -28,11 +44,19 @@ func BenchmarkPublish_struct(b *testing.B) {
 	}
 
 	t := DeclareTopic[d]()
+	outputChan := make(chan d, b.N)
+
+	t.Subscribe(func(ctx context.Context, p d) error {
+		outputChan <- p
+		return nil
+	})
 
 	for i := 0; i < b.N; i++ {
 		_ = t.Publish(context.Background(), d{
 			id:   1,
 			name: "test",
 		})
+
+		<-outputChan
 	}
 }
